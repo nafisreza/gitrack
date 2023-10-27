9e46007edfdce3808d0810a5ab653a58fed712ce
nafisreza <bigganinafis@gmail.com>
2023-10-28 04:13:18 +0600

    🔥 DOM Selectors linked

---
+++ script.js
@@ -0,0 +1,18 @@
+// DOM Selectors
+const btn = document.querySelector('#btn')
+const inputBox = document.querySelector('#input-box')
+const infoBox = document.querySelector('#info-box')
+
+const profilePic = document.querySelector('#profilePic')
+const followers = document.querySelector('#followers')
+const following = document.querySelector('#following')
+const repository = document.querySelector('#repository')
+const profileName = document.querySelector('#profileName')
+const profileUsername = document.querySelector('#profileUsername')
+const profileBio = document.querySelector('#profileBio')
+const profileLocation = document.querySelector('#profileLocation')
+const profileWorkplace = document.querySelector('#profileWorkplace')
+const websiteLink = document.querySelector('#websiteLink')
+const profileLink = document.querySelector('#profileLink')
+
+let username
